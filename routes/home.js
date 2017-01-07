83ca256ac2f20a24504c2d85debab1ea4c25fc11
Stephen Koch <stephen.koch@gmail.com>
2017-01-07 01:48:50 -0500

    Total reorganization

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,49 @@ var Express = require('express');
 
 var router = Express.Router();
 
+const alphabet = [
+    { letter: 'a', object: 'apple' },
+    { letter: 'a', object: 'alligator' },
+    { letter: 'b', object: 'banana' },
+    { letter: 'b', object: 'bear-alt' },
+    { letter: 'c', object: 'carrot' },
+    { letter: 'c', object: 'cow' },
+    { letter: 'd', object: 'duck' },
+    { letter: 'd', object: 'dandelion' },
+    { letter: 'e', object: 'eggplant' },
+    { letter: 'e', object: 'eggs' },
+    { letter: 'e', object: 'elephant' },
+    { letter: 'f', object: 'fig' },
+    { letter: 'f', object: 'fox' },
+    { letter: 'g', object: 'garlic' },
+    { letter: 'g', object: 'giraffe' },
+    { letter: 'g', object: 'goldfish' },
+    { letter: 'h', object: 'honey' },
+    { letter: 'h', object: 'honeydew' },
+    { letter: 'h', object: 'horse' },
+    { letter: 'i', object: 'iguana-alt' },
+    { letter: 'j', object: 'jaguar-alt' },
+    { letter: 'k', object: 'kangaroo' },
+    { letter: 'l', object: 'lion' },
+    { letter: 'm', object: 'meerkat-alt' },
+    { letter: 'n', object: 'nectarine' },
+    { letter: 'o', object: 'octopus-alt' },
+    { letter: 'p', object: 'penguin' },
+    { letter: 'q', object: 'quince' },
+    { letter: 'r', object: 'rattlesnake' },
+    { letter: 's', object: 'sheep' },
+    { letter: 't', object: 'tomato' },
+    { letter: 't', object: 'tree' },
+    { letter: 't', object: 'tarantula' },
+    { letter: 't', object: 'tiger-alt' },
+    { letter: 'u', object: 'umbrella' },
+    { letter: 'v', object: 'vulture' },
+    { letter: 'w', object: 'woodpecker' },
+    { letter: 'x', object: 'xylophone' },
+    { letter: 'y', object: 'yam' },
+    { letter: 'z', object: 'zebra' },
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('pages/home', {
@@ -9,6 +52,7 @@ router.get('/', function(req, res, next) {
         htmlClass: '',
         bodyClass: 'home fade',
         pageJs: 'home',
+        alphabet: alphabet,
     });
 });
 
--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -13,92 +13,99 @@ const ColorThief = require('color-thief-browser');
 
 const colorThief = new ColorThief();
 const hi = new HumanInput(window);
-const objects = {
-    a: ['alligator', 'apple'],
-    b: ['bear', 'banana'],
-    c: ['carrot', 'cow'],
-    d: ['dandelion', 'duck'],
-    e: ['eggplant', 'eggs', 'elephant'],
-    f: ['fig', 'fox'],
-    g: ['garlic', 'giraffe', 'goldfish'],
-    h: ['honeydew', 'honey', 'horse'],
-    i: ['iguana-alt'],
-    j: ['jaguar-alt'],
-    k: ['kangaroo'],
-    l: ['lion'],
-    m: ['meerkat-alt'],
-    n: ['nectarine'],
-    o: ['octopus-alt'],
-    p: ['penguin'],
-    q: ['quince'],
-    r: ['rattlesnake'],
-    s: ['sheep'],
-    t: ['tomato', 'tree', 'tarantula', 'tiger-alt'],
-    u: ['umbrella'],
-    v: ['vulture'],
-    w: ['woodpecker'],
-    x: ['xylophone'],
-    y: ['yam'],
-    z: ['zebra'],
-};
+
+var godMode;
 var signals;
 var currentLetter;
+var currentSelection;
 
 // console.log('home.js fs', fs.readFile);
 
+function _setActiveObject() {
+    const choices = $(`.letter-${currentLetter}`);
+    const previousSelection = currentSelection;
+    const selection = choices[Math.floor(Math.random() * choices.length)];
+    currentSelection = selection;
+    // console.log('selection', selection);
+
+    if (!$('.title').hasClass('fade')) {
+        $('.title').addClass('fade');
+    }
+    if (previousSelection) {
+        $(previousSelection).removeClass('in');
+        $(previousSelection).find('.word').removeClass('in');
+    }
+
+
+    setTimeout(() => {
+        const img = $(currentSelection).find('img');
+        const colors = colorThief.getPalette(img[0], 2);
+        $('.word').css({
+            color: `rgba(${colors[0][0]}, ${colors[0][1]}, ${colors[0][2]}, 1)`,
+        });
+        $('.home').css({
+            background: `linear-gradient(to bottom, rgba(${colors[1][0]}, ${colors[1][1]}, ${colors[1][2]},1) 0%,rgba(${colors[1][0]}, ${colors[1][1]}, ${colors[1][2]},1) 50%,rgba(${colors[2][0]}, ${colors[2][1]}, ${colors[2][2]},1) 50%,rgba(${colors[2][0]}, ${colors[2][1]}, ${colors[2][2]},1) 100%)`,
+        });
+        $('.content').css({
+            background: `linear-gradient(to bottom, rgba(${colors[1][0]}, ${colors[1][1]}, ${colors[1][2]},1) 0%,rgba(${colors[1][0]}, ${colors[1][1]}, ${colors[1][2]},1) 50%,rgba(${colors[2][0]}, ${colors[2][1]}, ${colors[2][2]},1) 50%,rgba(${colors[2][0]}, ${colors[2][1]}, ${colors[2][2]},1) 100%)`,
+        });
+        // if (currentLetter === 'w') {
+        //     console.log('colors[0]', colors[0]);
+        //     console.log('colors[1]', colors[1]);
+        //     console.log('colors[2]', colors[2]);
+        // }
+        $(currentSelection).addClass('in');
+    }, 100);
+}
+function _space(event) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    currentLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+    _setActiveObject();
+}
+
+function _toggleGodMode(event, key, code) {
+    godMode = !godMode;
+    console.log('godMode:', godMode);
+}
+
 function _whatKey(event, key, code) {
-    // console.log('event.type', event.type);
-    const regex = /^[A-Za-z]$/;
+    // if (!godMode) {
+    //     return;
+    // }
+    const regex = /^[a-z]$/;
     if (regex.test(key)) {
         // if (currentLetter === key) {
         //     return;
         // }
-        currentLetter = key;
-        if ($('.intro').length) {
-            $('.intro').addClass('fade');
-            return;
+        // if the keypress === current letter and it's not yet in
+        const sel = $(currentSelection);
+        if (key === currentLetter) {
+            if (!sel.find('.word').hasClass('in')) {
+                sel.find('.word').addClass('in');
+                // sel.find('img').removeClass('shake-horizontal shake-constant');
+            }
+        } else {
+            // sel.find('img').addClass('shake-horizontal shake-constant');
         }
+        // currentLetter = key;
 
-        $('.letter-object').removeClass('in');
-        // $('.letter').removeClass('in');
-        // $('.object').removeClass('in');
-        // $('.letter').html(currentLetter);
-    }
-    // hi.log.info(key, ' was pressed.  Here is the code:', code);
-}
-
-function _setObjectImage() {
-    // console.log('_setObjectImage');
-    $('.letter').html(currentLetter.toUpperCase());
-    const imageNames = objects[currentLetter];
-    const image = imageNames[Math.floor(Math.random() * imageNames.length)];
-    $('.object img').attr('src', `images/${currentLetter}/${image}.png`);
-    setTimeout(() => {
-        const img = $('.object').find('img');
-        const [red, blue, green] = colorThief.getColor(img[0]);
-        // console.log('>>>', colorThief.getPalette(img[0], 2));
-        const colors = colorThief.getPalette(img[0], 2);
-        // $('.letter').css({
-        //     backgroundColor: `rgba(${red}, ${blue}, ${green}, 0.5)`,
-        //     color: `rgb(${red}, ${blue}, ${green})`,
-        // });
-        $('.letter').css({
-            backgroundColor: `rgba(${colors[1][0]}, ${colors[1][1]}, ${colors[1][2]}, 1)`,
-            color: `rgba(${colors[0][0]}, ${colors[0][1]}, ${colors[0][2]}, 1)`,
-        });
-        $('.letter-object').css({
-            backgroundColor: `rgba(${colors[2][0]}, ${colors[2][1]}, ${colors[2][2]}, 1)`,
-        });
-        $('.letter-object').addClass('in');
-    }, 250);
-}
-
-function _objectClick(e) {
-    console.log('_objectClick');
-    if (!$('.letter').hasClass('in')) {
-        $('.letter').addClass('in');
+        // _setActiveObject();
     }
 }
+
+function _showLetter(event, key, code) {
+    console.log('_showLetter');
+    if (!$(currentSelection).find('.word').hasClass('in')) {
+        $(currentSelection).find('.word').addClass('in');
+    }
+}
+
+// function _objectClick(e) {
+//     if (!$(currentSelection).find('.word').hasClass('in')) {
+//         $(currentSelection).find('.word').addClass('in');
+//     }
+// }
 
 module.exports = {
     init: function(config) {
@@ -111,44 +118,50 @@ module.exports = {
         // console.log('$image[0]', $image[0]);
         // console.log('colorThief', colorThief.getColor($('.color-test').find('img')[0]));
 
+        // hi.on('ctrl-1', _showLetter);
+        // hi.on('ctrl-g', _toggleGodMode);
+        // you have to enable godMode in order to use the keys for display
         hi.on('keydown', _whatKey);
-        $('.object').click(_objectClick);
+        // default display for kids
+        hi.on('space', _space);
+        // $('.object').click(_objectClick);
 
-        transitions.init();
-        transitions.config.signals['onCssTransitionEnd'].add((o) => {
-            switch (o.data) {
-            case 'remove' : {
-                o.el.remove();
+        // transitions.init();
+        // transitions.config.signals['onCssTransitionEnd'].add((o) => {
+        //     switch (o.data) {
+        //     case 'remove' : {
+        //         o.el.remove();
 
-                _setObjectImage();
-                // $('.letter').html(currentLetter.toUpperCase());
-                // // $('.letter').addClass('in');
+        //         _setObjectImage();
+        //         // $('.letter').html(currentLetter.toUpperCase());
+        //         // // $('.letter').addClass('in');
 
-                // const imageNames = objects[currentLetter];
-                // const image = imageNames[Math.floor(Math.random() * imageNames.length)];
-                // $('.object img').attr('src', `images/${currentLetter}/${image}.png`);
-                // // $('.object').addClass('in');
+        //         // const imageNames = objects[currentLetter];
+        //         // const image = imageNames[Math.floor(Math.random() * imageNames.length)];
+        //         // $('.object img').attr('src', `images/${currentLetter}/${image}.png`);
+        //         // // $('.object').addClass('in');
 
-                // $('.letter-object').addClass('in');
-                break;
-            }
-            default :
-                console.log('no case for transition end');
-            }
-        });
-        transitions.config.signals['onCssTransitionPerpetual'].add((o) => {
-            switch (o.data) {
-            case 'show' :
-                // console.log('show', currentLetter);
-                // console.log('o.el', o.el);
-                if (!o.el.hasClass('in')) {
-                    _setObjectImage();
-                }
-                break;
-            default :
-                console.log('no case for transition perpetual');
-            }
-        });
+        //         // $('.letter-object').addClass('in');
+        //         break;
+        //     }
+        //     default :
+        //         console.log('no case for transition end');
+        //     }
+        // });
+        // transitions.config.signals['onCssTransitionPerpetual'].add((o) => {
+        //     switch (o.data) {
+        //     case 'show' :
+        //         // console.log('show', currentLetter);
+        //         // console.log('o.el', o.el);
+        //         if (!o.el.hasClass('in')) {
+        //             _setObjectImage();
+        //         }
+        //         break;
+        //     default :
+        //         console.log('no case for transition perpetual');
+        //     }
+        // });
+
         // hi.on('keydown', (event) => {
         //     hi.log.info('keydown');
         // });
